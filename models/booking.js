f9d0b569d552e0c3542af2e05b94b1e9b35c6e20
joshTalent <ntwarijosue5@gmail.com>
2025-09-26 12:32:41 +0300

    adding time to booking model

--- models/booking.js
+++ models/booking.js
@@ -6,9 +6,10 @@ const bookingSchema = new mongoose.Schema({
   phone: { type: String, required: true },
   service: { type: String, required: true },
   date: { type: String, required: true },
-  message : { type: String, required: true },
+  time: { type: String, required: true }, // <-- added time
+  message: { type: String, required: true },
 });
 
 const bookingModel = mongoose.model("booking", bookingSchema);
 
-export default bookingModel
+export default bookingModel;
